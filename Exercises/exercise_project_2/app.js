a9bbd1773124d47eccc9193db273ee749082368e
HussainGafrony <hgafrony@gmail.com>
2023-12-10 03:39:57 +0300

    end exercise 1

--- Exercises/exercise_project_2/app.js
+++
@@ -1,7 +0,0 @@
-const heightInput = document.getElementById('heightInput');
-const heightDisplay = document.getElementById('heightDisplay');
-
-heightDisplay.textContent = heightInput.value;
-heightInput.addEventListener('change', () => {
-  heightDisplay.textContent = heightInput.value;
-});
